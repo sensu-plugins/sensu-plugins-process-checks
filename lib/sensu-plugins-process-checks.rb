1ebbc148ed827e45ba1cc0e801afd3f9bc912b96
Matt Jones <mattjones@yieldbot.com>
2015-06-03 22:31:26 -0400

    deploy

--- lib/sensu-plugins-process-checks.rb
+++ lib/sensu-plugins-process-checks.rb
@@ -1,14 +1,1 @@
 require 'sensu-plugins-process-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsProcessChecks
-  class << self
-  end
-
-  class << self
-  end
-end
