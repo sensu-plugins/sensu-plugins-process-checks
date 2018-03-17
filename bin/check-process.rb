sensu-plugins/sensu-plugins-process-checks

commit 12a01d6ef4f873e31bd5ec8e6d844525602f1182

[CVE-2017-8418] - updating rubocop dependency.
Breaking Changes:
- removed ruby `< 2.1` support

Misc:
- appeased the cops

Signed-off-by: Ben Abrams <me@benabrams.it>

bin/check-process.rb
@@ -1,5 +1,4 @@
 #! /usr/bin/env ruby
-# encoding: UTF-8
 #
 # check-process
 #
@@ -209,7 +208,7 @@ class CheckProcess < Sensu::Plugin::Check::CLI
   #
   def on_cygwin?
     # #YELLOW
-    `ps -W 2>&1`; $CHILD_STATUS.exitstatus == 0 # rubocop:disable Semicolon
+    `ps -W 2>&1`; $CHILD_STATUS.zero? # rubocop:disable Semicolon
   end
 
   # Acquire all the proceeses on a system for further analysis
@@ -253,13 +252,13 @@ class CheckProcess < Sensu::Plugin::Check::CLI
     procs = acquire_procs
 
     if config[:file_pid] && (file_pid = read_pid(config[:file_pid]))
-      procs.reject! { |p| p[:pid].to_i != file_pid }
+      procs.select! { |p| p[:pid].to_i == file_pid }
     end
 
     procs.reject! { |p| p[:pid].to_i == $PROCESS_ID } unless config[:match_self]
     procs.reject! { |p| p[:pid].to_i == Process.ppid } unless config[:match_parent]
     procs.reject! { |p| p[:command] =~ /#{config[:exclude_pat]}/ } if config[:exclude_pat]
-    procs.reject! { |p| p[:command] !~ /#{config[:cmd_pat]}/ } if config[:cmd_pat]
+    procs.select! { |p| p[:command] =~ /#{config[:cmd_pat]}/ } if config[:cmd_pat]
     procs.select! { |p| p[:vsz].to_f > config[:vsz] } if config[:vsz]
     procs.select! { |p| p[:rss].to_f > config[:rss] } if config[:rss]
     procs.select! { |p| p[:cpu].to_f > config[:cpu_utilization] } if config[:cpu_utilization]
@@ -268,8 +267,8 @@ class CheckProcess < Sensu::Plugin::Check::CLI
     procs.reject! { |p| etime_to_esec(p[:etime]) <= config[:esec_over] } if config[:esec_over]
     procs.reject! { |p| cputime_to_csec(p[:time]) >= config[:cpu_under] } if config[:cpu_under]
     procs.reject! { |p| cputime_to_csec(p[:time]) <= config[:cpu_over] } if config[:cpu_over]
-    procs.reject! { |p| !config[:state].include?(p[:state]) } if config[:state]
-    procs.reject! { |p| !config[:user].include?(p[:user]) } if config[:user]
+    procs.select! { |p| config[:state].include?(p[:state]) } if config[:state]
+    procs.select! { |p| config[:user].include?(p[:user]) } if config[:user]
 
     msg = "Found #{procs.size} matching processes"
     msg += "; cmd /#{config[:cmd_pat]}/" if config[:cmd_pat]
@@ -287,7 +286,7 @@ class CheckProcess < Sensu::Plugin::Check::CLI
 
     if config[:metric]
       # #YELLOW
-      count = procs.map { |p| p[config[:metric]].to_i }.reduce { |a, b| a + b } # rubocop:disable SingleLineBlockParams
+      count = procs.map { |p| p[config[:metric]].to_i }.reduce { |a, b| a + b }
       msg += "; #{config[:metric]} == #{count}"
     else
       count = procs.size

.rubocop.yml
@@ -28,3 +28,9 @@ RegexpLiteral:
 
 Style/Documentation:
   Enabled: false
+
+# testing
+Metrics/BlockLength:
+  Enabled: true
+  Exclude:
+    - 'test/**/*'

.travis.yml
@@ -4,7 +4,6 @@ cache:
 install:
 - bundle install
 rvm:
-- 2.0
 - 2.1
 - 2.2
 - 2.3.0
@@ -27,7 +26,6 @@ deploy:
   on:
     tags: true
     all_branches: true
-    rvm: 2.0
     rvm: 2.1
     rvm: 2.2
     rvm: 2.3.0

Rakefile
@@ -7,12 +7,12 @@ require 'yard'
 require 'yard/rake/yardoc_task'
 require 'English'
 
-args = [:spec, :make_bin_executable, :yard, :rubocop, :check_binstubs]
+args = %i[spec make_bin_executable yard rubocop check_binstubs]
 
 YARD::Rake::YardocTask.new do |t|
-  OTHER_PATHS = %w().freeze
+  OTHER_PATHS = %w[].freeze
   t.files = ['lib/**/*.rb', 'bin/**/*.rb', OTHER_PATHS]
-  t.options = %w(--markup-provider=redcarpet --markup=markdown --main=README.md --files CHANGELOG.md)
+  t.options = %w[--markup-provider=redcarpet --markup=markdown --main=README.md --files CHANGELOG.md]
 end
 
 RuboCop::RakeTask.new

bin/check-process-restart.rb
@@ -1,5 +1,4 @@
 #! /usr/bin/env ruby
-#  encoding: UTF-8
 #
 #   check-process-restart
 #

bin/check-threads-count.rb
@@ -51,9 +51,13 @@ class ThreadsCount < Sensu::Plugin::Check::CLI
   # Takes a value to be tested as an integer. If a new Integer instance cannot be created from it, return 1.
   # See the comments on get_process_threads() for why 1 is returned.
   def test_int(i)
-    return Integer(i)
-  rescue
-    return 1
+    Integer(i)
+  rescue TypeError
+    1
+  rescue ArgumentError
+    1
+  rescue StandardError
+    1
   end
 
   # Takes a process struct from Sys::ProcTable.ps() as an argument
@@ -61,12 +65,12 @@ class ThreadsCount < Sensu::Plugin::Check::CLI
   # Returns the number of processes in those fields.
   # Otherwise, returns 1 as all processes are assumed to have at least one thread.
   def get_process_threads(p)
-    if p.respond_to?(:nlwp) # rubocop:disable Style/GuardClause
-      return test_int(p.nlwp)
+    if p.respond_to?(:nlwp)
+      test_int(p.nlwp)
     elsif p.respond_to?(:thread_count)
-      return test_int(p.thread_count)
+      test_int(p.thread_count)
     else
-      return 1
+      1
     end
   end
 

bin/metrics-per-process.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-bin_dir = File.expand_path(File.dirname(__FILE__))
+bin_dir = File.expand_path(__dir__)
 shell_script_path = File.join(bin_dir, File.basename($PROGRAM_NAME, '.rb') + '.py')
 
 exec shell_script_path, *ARGV

bin/metrics-process-status.rb
@@ -1,5 +1,4 @@
 #! /usr/bin/env ruby
-#  encoding: UTF-8
 #
 #   proc-status-metrics
 #
@@ -92,7 +91,7 @@ class ProcStatus < Sensu::Plugin::Metric::CLI::Graphite
   #
   def acquire_valid_pids(pgrep_output)
     res = pgrep_output.split("\n").map(&:strip)
-    pids = res.reject { |x| !x.integer? }
+    pids = res.select(&:integer?)
     pids
   end
 

commit c3e7ded0704d25fb674626f4f9203c3b234daa8d

fix and release 3.0.1 caused by #61

bin/check-process.rb
@@ -208,7 +208,7 @@ class CheckProcess < Sensu::Plugin::Check::CLI
   #
   def on_cygwin?
     # #YELLOW
-    `ps -W 2>&1`; $CHILD_STATUS.zero? # rubocop:disable Semicolon
+    `ps -W 2>&1`; $CHILD_STATUS.exitstatus.zero? # rubocop:disable Semicolon
   end
 
   # Acquire all the proceeses on a system for further analysis

lib/sensu-plugins-process-checks/version.rb
@@ -2,7 +2,7 @@ module SensuPluginsProcessChecks
   module Version
     MAJOR = 3
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end